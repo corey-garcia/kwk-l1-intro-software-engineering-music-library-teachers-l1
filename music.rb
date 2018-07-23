33177bd6716564849e3634d1e053fa487b567fae
Corey Garcia <corey.w.garcia@outlook.com>
2018-07-23 23:15:30 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -1,5 +1,7 @@
 
-
+hash = {}
+hash["Alicia"] = 1967
+hash["Bonnie"] = 1950
 
 
 #1. Define a method named `list_artists`. This method should use the `each_key` method to iterate over the hash and return all the artists (the keys)
